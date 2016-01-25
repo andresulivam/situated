fa487ff7bff1a98945b5d21e9a91ed0740a7e869
Andre Sulivam <asulivam@gmail.com>
2016-01-25 16:00:25 -0200

    Colocando colunas disponiveis para se adicionar aos eixos em ordem alfabética

--- js/plugin-functions.js
+++ js/plugin-functions.js
@@ -31,6 +31,12 @@ $(document).ready(function() {
 					}
 				}
 				var select = document.getElementById(CONST_SELECT_COLUMN);
+				// ordenando ordem alfabetica
+				arrayoptions.sort(function(a,b) {
+				    if (a.text > b.text) return 1;
+				    else if (a.text < b.text) return -1;
+				    else return 0
+				})
 				for(var i = 0; i < arrayoptions.length; i++){
 					// Adicionando todos os atributos/colunas ao combobox
 					select.appendChild(arrayoptions[i]);
